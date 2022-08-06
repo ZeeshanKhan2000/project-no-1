b17a5fe79dff65820e39a3f18f9ade649b0471c4
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2022-08-06 15:44:28 -0700

    Add files via upload

---
+++ Project-01/assets/js/script.js
@@ -0,0 +1,67 @@
+// Javascript topic
+
+// console.log //done
+// alert //done
+// prompt //done 
+// datatype //done
+// variable //done 
+// Number parstInt +
+// opertors 
+// Assignment Opertor
+// document write
+// mathmetical calculation
+// string concentation
+
+
+// Dom 
+
+
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+
+// document.querySelector
+// document.querySelectorAll
+
+
+/*const Form = document.getElementById("form");
+const FirstName = document.getElementById("f-name");
+const LastName = document.getElementById("l-name");
+const Phone = document.getElementById("phone");
+const Email = document.getElementById("email");
+
+
+
+Form.addEventListener("submit", createAccount)
+
+function createAccount(e) {
+    e.preventDefault();
+    
+    // console.log( FirstName.value)
+    // console.log( LastName.value)
+    // console.log( Phone.value)
+    // console.log(Email.value)
+
+    if(FirstName.value === ""){
+        alert("please fill first name")
+    }
+
+    
+    else if(LastName.value === ""){
+        alert("please fill last name")
+    }
+
+
+} */
+
+/*
+let person =  prompt ("please enter your name");
+
+
+
+
+
+
+
+
+// Notepade ++  
